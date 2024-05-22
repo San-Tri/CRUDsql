de947c9224aa3331ba1de6d7a55e79894beba384
unknown <stri80603@gmail.com>
2024-05-22 15:14:52 -0500

    CRUD

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alumno;
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    public function index()
+    {
+        $alumnos = Alumno::all();
+        return view('alumnos.index', compact('alumnos'));
+    }
+
+    public function create()
+    {
+        return view('alumnos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'email' => 'required|email|unique:alumnos',
+        ]);
+
+        Alumno::create($request->all());
+
+        return redirect()->route('alumnos.index')
+                         ->with('success', 'Alumno creado exitosamente.');
+    }
+
+    public function show(Alumno $alumno)
+    {
+        return view('alumnos.show', compact('alumno'));
+    }
+
+    public function edit(Alumno $alumno)
+    {
+        return view('alumnos.edit', compact('alumno'));
+    }
+
+    public function update(Request $request, Alumno $alumno)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'email' => 'required|email|unique:alumnos,email,' . $alumno->id,
+        ]);
+
+        $alumno->update($request->all());
+
+        return redirect()->route('alumnos.index')
+                         ->with('success', 'Alumno actualizado exitosamente.');
+    }
+
+    public function destroy(Alumno $alumno)
+    {
+        $alumno->delete();
+
+        return redirect()->route('alumnos.index')
+                         ->with('success', 'Alumno eliminado exitosamente.');
+    }
+}
+
